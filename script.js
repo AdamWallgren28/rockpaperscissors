a1476a2518f81e57f10b3e28ccef6c8ca35aed81
AdamWallgren28 <mattef91@hotmail.com>
2024-01-01 23:02:37 +0100

    adds 1 round of rock-paper-scissors

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function getComputerChoice(){
-        return Math.floor(Math.random() * 3);
+    let x = Math.floor(Math.random() * 3);
+        if (x==0){
+            return "Rock";
+        }else if(x==1){
+            return "Paper";
+        }else if (x==2){
+            return "Scissors";
+        }
 }
-console.log(getComputerChoice());
+
+function rockPaperScissors(playerSelection, computerSelection){
+    let playerHand = playerSelection.toLowerCase();
+    if (playerHand == "rock" && computerSelection == "Rock"){
+        return "TIE! ...try again!";
+    }else if (playerHand == "rock" && computerSelection == "Paper"){
+        return "YOU LOSE!";
+    }else if (playerHand == "rock" && computerSelection == "Scissors"){
+        return "YOU WIN!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(rockPaperScissors(playerSelection, computerSelection));
+
