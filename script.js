a7a4b652fadd7248a96fd52076321667acf0d959
AdamWallgren28 <mattef91@hotmail.com>
2024-01-01 23:47:44 +0100

    makes game ready to play online

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getComputerChoice(){
         }
 }
 
-function rockPaperScissors(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
     let playerHand = playerSelection.toLowerCase();
     if (playerHand == "rock" && computerSelection == "Rock"){
         return "TIE! ...try again!";
@@ -17,10 +17,22 @@ function rockPaperScissors(playerSelection, computerSelection){
         return "YOU LOSE!";
     }else if (playerHand == "rock" && computerSelection == "Scissors"){
         return "YOU WIN!"
+    }else if (playerHand == "paper" && computerSelection == "Rock"){
+        return "YOU WIN!";
+    }else if (playerHand == "paper" && computerSelection == "Paper"){
+        return "TIE! ...try again!";
+    }else if (playerHand == "paper" && computerSelection == "Scissors"){
+        return "YOU LOSE!";
+    }else if (playerHand == "scissors" && computerSelection == "Rock"){
+        return "YOU LOSE!";
+    }else if (playerHand == "scissors" && computerSelection == "Paper"){
+        return "YOU WIN!";
+    }else if (playerHand == "scissors" && computerSelection == "Scissors"){
+        return "TIE! ...try again!";
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt("Welcome! Pick: Rock, Paper or Scissors!");
 const computerSelection = getComputerChoice();
-console.log(rockPaperScissors(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
