f3bc520c51e8af534c45242b4f8b6b0b1e2f9c06
AdamWallgren28 <mattef91@hotmail.com>
2024-01-02 00:26:55 +0100

    cleans up func playRound

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 function getComputerChoice(){
     let x = Math.floor(Math.random() * 3);
         if (x==0){
-            return "Rock";
+            return "rock";
         }else if(x==1){
-            return "Paper";
+            return "paper";
         }else if (x==2){
-            return "Scissors";
+            return "scissors";
         }
 }
 
 function playRound(playerSelection, computerSelection){
     let playerHand = playerSelection.toLowerCase();
-    if (playerHand == "rock" && computerSelection == "Rock"){
-        return "TIE! ...try again!";
-    }else if (playerHand == "rock" && computerSelection == "Paper"){
+    if (playerHand == "rock" && computerSelection == "paper"){
         return "YOU LOSE!";
-    }else if (playerHand == "rock" && computerSelection == "Scissors"){
+    }else if (playerHand == "rock" && computerSelection == "scissors"){
         return "YOU WIN!"
-    }else if (playerHand == "paper" && computerSelection == "Rock"){
+
+    }else if (playerHand == "paper" && computerSelection == "rock"){
         return "YOU WIN!";
-    }else if (playerHand == "paper" && computerSelection == "Paper"){
-        return "TIE! ...try again!";
-    }else if (playerHand == "paper" && computerSelection == "Scissors"){
+    }else if (playerHand == "paper" && computerSelection == "scissors"){
         return "YOU LOSE!";
-    }else if (playerHand == "scissors" && computerSelection == "Rock"){
+
+    }else if (playerHand == "scissors" && computerSelection == "rock"){
         return "YOU LOSE!";
-    }else if (playerHand == "scissors" && computerSelection == "Paper"){
+    }else if (playerHand == "scissors" && computerSelection == "paper"){
         return "YOU WIN!";
-    }else if (playerHand == "scissors" && computerSelection == "Scissors"){
+
+    }else if (playerHand == computerSelection){
         return "TIE! ...try again!";
     }
 }
 
-const playerSelection = prompt("Welcome! Pick: Rock, Paper or Scissors!");
+const playerSelection = prompt("Pick: Rock, Paper or Scissors!");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
-
