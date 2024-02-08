2f585302b910795997d8571438ed4ebc0e7be5d0
AdamWallgren28 <mattef91@hotmail.com>
2024-02-08 18:24:39 +0100

    ads UI to rounds and Score

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+// Definerar knappar
+let rockBtn = document.querySelector('#rockBtn');
+let paperBtn = document.querySelector('#paperBtn');
+let scissorBtn = document.querySelector('#scissorBtn');
+let newGameBtn = document.querySelector('#newGameBtn');
+// Definerar display för resultat
+let roundResult = document.querySelector('#roundResult');
+let playerScore = document.querySelector('#playerScore');
+let compScore = document.querySelector('#compScore');
+let winner = document.querySelector('#winner');
+
+
+// Generera datorns val
 function getComputerChoice(){
     let x = Math.floor(Math.random() * 3);
         if (x==0){
@@ -10,53 +23,82 @@ function getComputerChoice(){
         }
 }
 
+// Omgånsresultat baserat på klientval (och dators val)
 function playRound(playerSelection, computerSelection){
     let playerHand = playerSelection.toLowerCase();
-    if (playerHand == "rock" && computerSelection == "paper"){
-        return "YOU LOSE!";
-    }else if (playerHand == "rock" && computerSelection == "scissors"){
-        return "YOU WIN!"
-
-    }else if (playerHand == "paper" && computerSelection == "rock"){
-        return "YOU WIN!";
-    }else if (playerHand == "paper" && computerSelection == "scissors"){
-        return "YOU LOSE!";
-
-    }else if (playerHand == "scissors" && computerSelection == "rock"){
-        return "YOU LOSE!";
-    }else if (playerHand == "scissors" && computerSelection == "paper"){
-        return "YOU WIN!";
-
-    }else if (playerHand == computerSelection){
-        return "TIE! ...try again!";
-    }
-}
-
-function game(){
-    let pScore = 0;
-    let cScore = 0;
-    let i = 1;
-
-        for (i=1 ; i<6; i++){
-            let r=playRound(prompt("Pick: Rock, Paper or Scissors! (Round" + i + ")"), getComputerChoice());
-            if (r == "YOU WIN!"){
-                pScore++;
-                console.log("You: " + pScore + " Computer: " + cScore);
-            }else if (r == "YOU LOSE!"){
-                cScore++;
-                console.log("You: " + pScore + " Computer: " + cScore);
-            }else if (r == "TIE! ...try again!"){
-                console.log("TIE! ...try again!");
-                i--;
-            }
-            
-            if (pScore == 3){
-                return "YOU WIN!";
-                break
-            }else if (cScore == 3) {
-                return "YOU LOOSE";
-                break
-            }
+        if (playerHand == "rock" && computerSelection == "paper"){
+            return"YOU LOSE!";
+        }else if (playerHand == "rock" && computerSelection == "scissors"){
+            return"YOU WIN!";
+        }else if (playerHand == "paper" && computerSelection == "rock"){
+            return"YOU WIN!";
+        }else if (playerHand == "paper" && computerSelection == "scissors"){
+            return "YOU LOSE!";
+        }else if (playerHand == "scissors" && computerSelection == "rock"){
+            return"YOU LOSE!";
+        }else if (playerHand == "scissors" && computerSelection == "paper"){
+            return"YOU WIN!";
+        } else if (playerHand == computerSelection) {
+            return "TIE! ...try again!";
         }
 }
-console.log(game());
+
+// Knappfunktioner (per omgång)
+rockBtn.addEventListener('click', function () {
+    roundResult.textContent = '';
+    setTimeout(function() {
+        roundResult.textContent = playRound('rock', getComputerChoice());
+    }, 50);
+});
+paperBtn.addEventListener('click', function () {
+    roundResult.textContent = '';
+    setTimeout(function() {
+        roundResult.textContent = playRound('paper', getComputerChoice());
+    }, 50);
+});
+scissorBtn.addEventListener('click', function () {
+    roundResult.textContent = '';
+    setTimeout(function() {
+        roundResult.textContent = playRound('scissors', getComputerChoice());
+    }, 50);
+});
+
+
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorBtn.disabled = true;
+    newGameBtn.style.backgroundColor = 'aquamarine';
+}
+
+// Bäst av 5
+function game() {
+    let pScore = 0;
+    let cScore = 0;
+    let observer = new MutationObserver(function(mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.target.textContent === "YOU WIN!") {
+                pScore++;
+                playerScore.textContent = pScore;
+            } else if (mutation.target.textContent === "YOU LOSE!") {
+                cScore++;
+                compScore.textContent = cScore;
+            }
+
+            if (pScore === 3) {
+                winner.textContent = "YOU WON!";
+                disableButtons();
+            } else if (cScore === 3) {
+                winner.textContent = "YOU LOST...";
+                disableButtons();
+            } 
+        }
+    });
+    observer.observe(roundResult, { childList: true });
+}
+game();
+
+function resetGame() {
+    location.reload();
+}
+newGameBtn.addEventListener('click', resetGame);
